903bd01a62ebde2dbe2c9ce5ef2d1d88bca7b3c0
shondriane <shondriane.wise@yahoo.com>
2022-10-28 01:53:30 -0400

    working on search

--- my-travel-log/src/pages/Home.js
+++ my-travel-log/src/pages/Home.js
@@ -51,7 +51,7 @@ const month = upcoming.getUTCMonth() + 1;
 const day = upcoming.getUTCDate();
 const withHyphens = [year, month, day].join('-');
 activityDetails.map((activity)=>{
-  if (withHyphens <= `${activity.date}` && `$activity.date}` !== "undefined"){
+  if (withHyphens <= `${activity.date}` && `${activity.date}` !== "undefined"){
     
  console.log( `you have an upcoming activity for ${activity.name} on ${activity.date}`)
 }})
